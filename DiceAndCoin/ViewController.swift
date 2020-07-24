26b5d067d364be18f99e45f4603d85f5a55faf2d
Nikolay <nickolay.i.tkachenko@gmail.com>
2020-07-24 11:36:03 +0300

    Initial Commit

---
+++ DiceAndCoin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceAndCoin
+//
+//  Created by Nikolay Tkachenko on 24.07.2020.
+//  Copyright © 2020 Nikolay Tkachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
